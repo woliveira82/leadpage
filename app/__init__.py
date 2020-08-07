a2590b2bdd562244b8ba32b71ef6a148b593d7fd
woliveira82 <woliveira82@gmail.com>
2020-08-06 22:56:47 -0300

    Update app, add error handler in jwt

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,7 @@ from flask_jwt_extended import JWTManager
 
 def handler_error(app):
     
-    
+
     @app.errorhandler(HTTPException)
     def handler_error(error):
         return ResponseException(error.name, status=error.code).to_dict(), error.code
@@ -27,6 +27,23 @@ def handler_error(app):
         data = str(error)
         return ResponseException(data, status=500).to_dict(), 500
 
+
+def handler_jwt_error(jwt):
+
+
+    @jwt.expired_token_loader
+    @jwt.claims_verification_failed_loader
+    @jwt.expired_token_loader
+    @jwt.invalid_token_loader
+    @jwt.needs_fresh_token_loader
+    @jwt.revoked_token_loader
+    @jwt.token_in_blacklist_loader
+    @jwt.unauthorized_loader
+    @jwt.user_loader_error_loader
+    def token_callback(expired_token):
+        print(expired_token)
+        return ResponseException(str(expired_token), status=401).to_dict(), 401
+
 app = Flask(__name__, instance_relative_config=True)
 handler_error(app)
 app.config.from_pyfile('config.py')
@@ -34,6 +51,7 @@ app.config.from_pyfile('config.py')
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 jwt = JWTManager(app)
+handler_jwt_error(jwt)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
