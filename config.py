d3808b251609e8c67b2d57cd88b59ab8d2bdae10
woliveira82 <woliveira82@gmail.com>
2020-08-07 21:17:39 -0300

    Update database connector to MySQL

--- config.py
+++ config.py
@@ -1,11 +1,5 @@
-import os.path
-
-def sqlite_path():
-    sqlite_path = os.path.dirname(os.path.abspath(__file__))
-    return sqlite_path
-
 ENV = 'production'
 DEBUG = False
-SQLALCHEMY_DATABASE_URI = f'sqlite:///{sqlite_path()}storage.db'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://username:password@localhost/db_name'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 JWT_SECRET_KEY = 'super-secret'
