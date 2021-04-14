3d62ee8f660536729eb5130aadf4d45769c3d012
woliveira82@gmail.com <woliveira82@gmail.com>
2021-04-13 22:55:22 -0300

    New templates and routes

--- app/view/views.py
+++ app/view/views.py
@@ -2,16 +2,49 @@ from flask import render_template
 
 from app import app
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/admin')
-def admin():
+@app.route('/login')
+def login():
     return render_template('admin.html')
 
 
-@app.route('/unsubscribe')
-def unsubscribe():
-    return render_template('unsubscribe.html')
+@jwt_required
+@app.route('/project-list')
+def project_list():
+    return render_template('project-list.html')
+
+
+@jwt_required
+@app.route('/project')
+def project():
+    return render_template('project.html')
+
+
+@jwt_required
+@app.route('/lead-list')
+def lead_list():
+    return render_template('lead-list.html')
+
+
+@jwt_required
+@app.route('/email-list')
+def email_list():
+    return render_template('email-list.html')
+
+
+@jwt_required
+@app.route('/email')
+def email():
+    return render_template('email.html')
+
+
+@jwt_required
+@app.route('/lead-list')
+def lead_list():
+    return render_template('lead-list.html')
+
