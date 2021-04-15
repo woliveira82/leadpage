acc2dd0635f7e63f634b54cb9a7dd11dc15ae614
Weiner Oliveira <woliveira82@gmail.com>
2021-04-15 00:02:46 -0300

    Refectored /api/login, added webargs

--- app/apis/login/login_apis.py
+++ app/apis/login/login_apis.py
@@ -1,23 +1,24 @@
 from app.apis.login import login
-from app.inc import Parser, Response, ResponseException, Valid
+from app.inc import Response, ResponseException
 from app.model import User
 from flask import request
 from flask_jwt_extended import create_access_token
+from webargs import fields, validate
+from webargs.flaskparser import parser
 
 
 @login.route('/', methods=['POST'])
 def post_login():
-        data = Parser(request).parse({
-            'email': Valid(required=True),
-            'password': Valid(required=True)
-        })
-        users = User.read(query=data)
-        
-        if len(users) == 0:
-            raise ResponseException('Email or password is wrong', status=401)
+    json = parser.parse({
+        'email': fields.Email(required=True),
+        'password': fields.Str(required=True)
+    }, request)
+    user = User.session.filter_by(**json).first()
+    if not user:
+        raise ResponseException('Email or password is wrong', status=401)
 
-        access_token = create_access_token(identity=data['email'])
-        return Response({'access_token': access_token}).to_dict()
+    access_token = create_access_token(identity=user.email)
+    return Response({'access_token': access_token}).to_dict()
 
 
 @login.route('/', methods=['POST'])
